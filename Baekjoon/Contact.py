24e3af38f02f72f5cb033c63bf38be8e749e0091
parkmonty <456whdtjr@gmail.com>
2023-09-26 22:10:35 +0900

    second try (success)

--- Baekjoon/Contact.py
+++ Baekjoon/Contact.py
@@ -1,50 +1,28 @@
 # 문제 출처 : https://www.acmicpc.net/problem/1013
 # 전파 패턴 : (100+1+ | 01)+
+
 import sys
+# 정규 표현식 : 특정한 규칙을 가진 문자열 패턴을 사용하는 방식
+# github 정리 : https://github.com/parkmonty/W_Pile/blob/main/SAI/%EC%A0%95%EA%B7%9C%ED%91%9C%ED%98%84%EC%8B%9D(re).ipynb
+import re
 
 
 T = int(sys.stdin.readline())
-temp = 0
+result = []
 
-while temp < T:
-    temp += 1
-    s_num = sys.stdin.readline()
+# re.compile("패턴")
+for _ in range(T):
+    num_s = sys.stdin.readline().strip()
+    pattern = re.compile("(100+1+|01)+")
+    match_result = pattern.fullmatch(num_s)
     
-    index = 0
-    while index < len(s_num):
-        pre_index = index.copy()
+    if match_result:
+        result.append("YES")
+    else:
+        result.append("NO")
         
-        if index + 2 < len(s_num):
-            if s_num[index : index + 3] == "100":
-                index_1 = s_num[index + 3:].find("1")
-                index_0 = s_num[index_1:].find("0")
-                
-                if index_1 == -1:
-                    print("NO")
-                    break
-                elif index_0 == -1:
-                    print("YES")
-                    break
-                else:
-                    if index_0 - index_1 == 1:
-                        index = index_0
-                    else:
-                        if len(s_num) > index_0 + 1:
-                            if s_num[index_0 + 1] == "1":
-                                index = index_0
-                            else:
-                                index = index_0 - 1
-        
-        if index + 1 < len(s_num):
-            if s_num[index : index + 2] == "01" and index + 2 == len(s_num):
-                print("YES")
-                break
-            elif s_num[index : index + 2] == "01" and index + 2 < len(s_num):
-                index += 2
-                
-        if pre_index == index:
-            print("NO")
-            break
-        
-        if index + 1 == len(s_num):
-            print("YES")
+for i in result:
+    # 속도 향상을 위해 print 대신 sys.stdout.write 사용
+    # print -> 메모리 적음 / 속도 느림 / 줄바꿈하여 출력
+    # sys.stdout.write() -> 메모리 큼 / 속도 빠름 / 줄바꿈없이 바로이어서 출력
+    sys.stdout.write(i + "\n")
