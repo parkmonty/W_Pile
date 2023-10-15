16f897c567ae03ae2a57786849d830f260694143
parkmonty <456whdtjr@gmail.com>
2023-10-15 11:59:39 +0900

    first (시간 초과)

---
+++ Baekjoon/피보나치 함수.py
@@ -0,0 +1,29 @@
+# 문제 출처 : https://www.acmicpc.net/problem/1003
+import sys
+
+
+def count(N):
+    print(str(count_0(N)) + " " + str(count_1(N)))
+
+def count_0(N):
+    if N == 0:
+        return 1
+    elif N == 1:
+        return 0
+    else:
+        return count_0(N - 1) + count_0(N - 2)
+
+def count_1(N):
+    if N == 1:
+        return 1
+    elif N == 0:
+        return 0
+    else:
+        return count_1(N - 1) + count_1(N - 2)
+    
+    
+T = []
+T.append(sys.stdin.readline())
+
+for i in T:
+    count(int(i))
