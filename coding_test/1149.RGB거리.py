ca2591b982805c44770685c00886bfa1c31ccebd
원종석 <456whdtjr@gmail.com>
2024-03-19 22:56:34 +0900

    IndexError: list index out of range

---
+++ coding_test/1149.RGB거리.py
@@ -0,0 +1,15 @@
+import sys
+
+house_count = int(sys.stdin.readline().strip())
+RGB_price = [list(map(int, sys.stdin.readline().split())) for _ in range(house_count)]
+
+min_price = list()
+min_price[0] = RGB_price[0]
+
+for i in range(1, house_count):
+    min_price[i][0] = min(min_price[i - 1][1], min_price[i - 1][2]) + RGB_price[i][0]
+    min_price[i][1] = min(min_price[i - 1][0], min_price[i - 1][2]) + RGB_price[i][1]
+    min_price[i][2] = min(min_price[i - 1][0], min_price[i - 1][1]) + RGB_price[i][2]
+
+min_price = min(min_price)
+print(min_price)
